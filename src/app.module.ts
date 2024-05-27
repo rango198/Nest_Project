e09934ffad954d68addad1161b345cea1093d35d
rango198 <137562811+rango198@users.noreply.github.com>
2024-05-27 12:47:43 +0300

    swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [ProductsModule, MongooseModule.forRoot('mongodb+srv://DataBase:23pm89ecio@cluster0.5efshsq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')],
+  imports: [ConfigModule.forRoot(), ProductsModule, MongooseModule.forRoot(process.env.MONGODB_URI)],
   controllers: [AppController],
   providers: [AppService],
 })
